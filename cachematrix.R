1adef8c1787e03dcf05e36581129b8734b95fbcf
Ivan Mikhailov <ioanni.m@gmail.com>
2016-02-03 18:57:35 +0300

    Rewrote the makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file represents two functions for caching of initial and
+## inversed matrices
 
-## Write a short comment describing this function
+## The following function creates a "matrix" object, which is really
+## a list containing functions to:
+## 1. set the value of the matrix
+## 2. get the value of the matrix 
+## 3. set the value of inversed matrix 
+## 4. get the value of inversed matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-  x
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inversed) inv <<- inversed
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## The following function returns a matrix that is as inversed 
+## version of initial matrix
 
 cacheSolve <- function(x, ...) {
         solve(x)
